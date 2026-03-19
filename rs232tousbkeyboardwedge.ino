7846463ce7ffdd515fb67fb911d5839c855d79b4
CallenMagnuson <47834262+CallenMagnuson@users.noreply.github.com>
2026-03-19 08:12:54 -0500

    Formatting fixes

--- rs232tousbkeyboardwedge.ino
+++ rs232tousbkeyboardwedge.ino
@@ -40,102 +40,89 @@ void setup() {
 }
 
 void loop() {
+  // Check to see if anything is available in the serial receive buffer
+  while (mySerial.available() > 0) {
+    // Create a place to hold the incoming message
+    static char message[MAX_MESSAGE_LENGTH];
+    static unsigned int message_pos = 0;
 
- // Check to see if anything is available in the serial receive buffer
- while (mySerial.available() > 0)
- {
-   // Create a place to hold the incoming message
-   static char message[MAX_MESSAGE_LENGTH];
-   static unsigned int message_pos = 0;
+    // Read the next available byte in the serial receive buffer
+    char inByte = mySerial.read();
 
-   // Read the next available byte in the serial receive buffer
-   char inByte = mySerial.read();
+    // Message coming in (check not terminating character) and guard for over message size
+    if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) ) {
+      // Add the incoming byte to our message
+      message[message_pos] = inByte;
+      message_pos++;
+    }
 
-   // Message coming in (check not terminating character) and guard for over message size
-   if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) )
-   {
-     // Add the incoming byte to our message
-     message[message_pos] = inByte;
-     message_pos++;
-   }
+    // Full message received...
+    else {
+      // Add null character to string
+      message[message_pos] = '\0';
 
-   // Full message received...
-   else
-   {
-     // Add null character to string
-     message[message_pos] = '\0';
+      // Print the message (or do other things)
+      Serial.print("Message Received: ");
+      Serial.println(message);
 
-     // Print the message (or do other things)
-     Serial.print("Message Received: ");
-     Serial.println(message);
-
-     for (int n = 0; n < message_pos - 1; n++)
-     {
-      if (message[n] == (char)129)
-      {
-        // Unused ASCII space is being used to receive control keys 
-        // Decimal 129 - Hex 81 
-        Keyboard.press(KEY_LEFT_CTRL);
-        Serial.println("CTRL Pressed");
-      }
-      else if (message[n] == (char)141)
-      {
-        // Unused ASCII space is being used to receive control keys 
-        // Decimal 141 - Hex 8D 
-        Keyboard.press(KEY_LEFT_ALT);
-        Serial.println("ALT Pressed");
-      }
-      else if (message[n] == (char)143)
-      {
-        // Unused ASCII space is being used to receive control keys 
-        // Decimal 143 - Hex 8F 
-        Keyboard.press(KEY_LEFT_GUI);
-        Serial.println("GUI Key Pressed");
-      }
-      else if (message[n] == (char)144)
-      {
-        // Unused ASCII space is being used to receive control keys 
-        // Decimal 144 - Hex 90 
-        Keyboard.press(KEY_LEFT_SHIFT);
-        Serial.println("Shift Pressed");
-      }
-      else if (message[n] == (char)157)
-      {
-        // Unused ASCII space is being used to receive control keys 
-        // Decimal 157 - Hex 9D 
-        Keyboard.press(KEY_TAB);
-        Serial.println("Tab Pressed");
-      }
-      else
-      {
-        // Check to see if the first position of the serial input is a modifier, if not, then we press and release the key to type normally
-        if (message[0] == (char)157 || message[0] == (char)144 || message[0] == (char)143 || message[0] == (char)141 || message[0] == (char)129)
-        {
-          Keyboard.press((char)message[n]);
-          Keyboard.release((char)message[n]);
-          Serial.print((char)message[n]);
-          Serial.println(" Typed");
+      for (int n = 0; n < message_pos - 1; n++) {
+        if (message[n] == (char)129) {
+          // Unused ASCII space is being used to receive control keys 
+          // Decimal 129 - Hex 81 
+          Keyboard.press(KEY_LEFT_CTRL);
+          Serial.println("CTRL Pressed");
         }
-        else
-        {
-          // Send an ASCII character within the used character space
-          Keyboard.press((char)message[n]);
-          Serial.print((char)message[n]);
-          Serial.println(" Pressed");
+        else if (message[n] == (char)141) {
+          // Unused ASCII space is being used to receive control keys 
+          // Decimal 141 - Hex 8D 
+          Keyboard.press(KEY_LEFT_ALT);
+          Serial.println("ALT Pressed");
+        }
+        else if (message[n] == (char)143) {
+          // Unused ASCII space is being used to receive control keys 
+          // Decimal 143 - Hex 8F 
+          Keyboard.press(KEY_LEFT_GUI);
+          Serial.println("GUI Key Pressed");
+        }
+        else if (message[n] == (char)144) {
+          // Unused ASCII space is being used to receive control keys 
+          // Decimal 144 - Hex 90 
+          Keyboard.press(KEY_LEFT_SHIFT);
+          Serial.println("Shift Pressed");
+        }
+        else if (message[n] == (char)157) {
+          // Unused ASCII space is being used to receive control keys 
+          // Decimal 157 - Hex 9D 
+          Keyboard.press(KEY_TAB);
+          Serial.println("Tab Pressed");
+        }
+        else {
+          // Check to see if the first position of the serial input is a modifier, if not, then we press and release the key to type normally
+          if (message[0] == (char)157 || message[0] == (char)144 || message[0] == (char)143 || message[0] == (char)141 || message[0] == (char)129) {
+            Keyboard.press((char)message[n]);
+            Keyboard.release((char)message[n]);
+            Serial.print((char)message[n]);
+            Serial.println(" Typed");
+          }
+          else {
+            // Send an ASCII character within the used character space
+            Keyboard.press((char)message[n]);
+            Serial.print((char)message[n]);
+            Serial.println(" Pressed");
+          }
         }
       }
-     }
 
-     // Release Keys after adding modifiers
-     delay(10);
-     Keyboard.releaseAll();
-     Serial.println("Keys Released");
+        // Release Keys after adding modifiers
+      delay(10);
+      Keyboard.releaseAll();
+      Serial.println("Keys Released");
 
-     // Send OK response on mySerial to confirm command was executed
-     mySerial.println("OK");
+      // Send OK response on mySerial to confirm command was executed
+      mySerial.println("OK");
 
-     // Reset for the next message
-     message_pos = 0;
-   }
- }
+      // Reset for the next message
+      message_pos = 0;
+    }
+  }
 }
